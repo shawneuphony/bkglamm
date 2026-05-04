3650fc1d163494ff02bd0f22392f8d95a7be70c4
shawneuphony <contactlefikakoone@gmail.com>
2026-05-04 16:04:34 +0200

    updates

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -9,7 +9,6 @@ import { GenerateTitle, GenerateURL } from '@payloadcms/plugin-seo/types'
 import { FixedToolbarFeature, HeadingFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
 import { searchFields } from '@/search/fieldOverrides'
 import { beforeSyncWithSearch } from '@/search/beforeSync'
-import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 import { Page, Post } from '@/payload-types'
 import { getServerSideURL } from '@/utilities/getURL'
@@ -47,13 +46,6 @@ export const plugins: Plugin[] = [
       },
     },
   }),
-  vercelBlobStorage({
-  enabled: true,
-  collections: {
-    media: true,
-  },
-  token: process.env.BLOB_READ_WRITE_TOKEN || '',
-}),
   nestedDocsPlugin({
     collections: ['categories'],
     generateURL: (docs) => docs.reduce((url, doc) => `${url}/${doc.slug}`, ''),
