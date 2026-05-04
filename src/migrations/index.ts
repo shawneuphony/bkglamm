000f778c06c68858787f4c63c7d4fdc04ac3d28d
shawneuphony <contactlefikakoone@gmail.com>
2026-05-04 12:52:41 +0200

    chore: add initial Turso migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260504_104834 from './20260504_104834';
+
+export const migrations = [
+  {
+    up: migration_20260504_104834.up,
+    down: migration_20260504_104834.down,
+    name: '20260504_104834'
+  },
+];
